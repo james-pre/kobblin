00bc8adc399c3e37c1faf3f9586ccee74d262c73
James Prevett <jp@jamespre.dev>
2024-11-30 17:54:53 -0600

    Began implementing module stuff

--- src/process.ts
+++ src/process.ts
@@ -1,18 +1,49 @@
 import { bindContext, type BoundContext } from '@zenfs/core';
+import { EventEmitter } from 'eventemitter3';
 import 'ses';
+import type { CompartmentOptions, ModuleDescriptor } from 'ses';
+import { List } from 'utilium';
 
-function __import() {}
+lockdown();
 
-export class Process {
-	protected compartment: Compartment = new Compartment({
-		globals: {
-			__import,
-		},
-	});
+export class Process extends EventEmitter {
+	protected compartmentOptions(): CompartmentOptions & { __options__: true } {
+		const p = this;
+		return {
+			__options__: true,
+			moduleMapHook(specifier: string): ModuleDescriptor | undefined {
+				if (!specifier.startsWith('sys:')) return undefined;
+				switch (specifier.slice(4)) {
+					case 'fs':
+						return p.fs_context;
+				}
+			},
+			importHook(): Promise<ModuleDescriptor> {
+				return;
+			},
+			importNowHook(): ModuleDescriptor {
+				return;
+			},
+			get name() {
+				return p.name;
+			},
+		};
+	}
 
-	protected fs_context: BoundContext;
+	public name?: string;
 
-	public constructor(public readonly parent?: Process) {
-		this.fs_context = bindContext('/');
+	protected compartment = new Compartment(this.compartmentOptions());
+
+	protected readonly fs_context: BoundContext;
+
+	public readonly children = new List<Process>();
+
+	public constructor(
+		protected code: string,
+		public readonly parent?: Process
+	) {
+		super();
+		this.fs_context = bindContext(this.parent?.fs_context.root ?? '/');
+		this.compartment.evaluate(code);
 	}
 }
---
+++ src/sys.d.ts
@@ -0,0 +1,5 @@
+// Built-in kernel modules
+
+declare module 'sys:fs' {
+	export * from 'fs';
+}
